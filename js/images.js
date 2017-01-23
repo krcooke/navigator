1ed0906f85a5a896d9d733b4f131c751fb79ca36
Kieron Cooke <kieron_cooke@hotmail.com>
2017-01-23 17:00:46 +0000

    Updated layout example

--- js/images.js
+++ js/images.js
@@ -1,25 +1,27 @@
 function displayAllImages() {
     var folder = "images/";
 
-$.ajax({
-    url : folder,
-    cache: false,
-    success: function (data) {
-        $(data).find("a").attr("href", function (i, val) {
-            console.log(val)
-            if( val.match(/\.(jpe?g|png|gif)$/) ) {
-                console.log(val)
-                var img = new Image();
-                img.src = val;
-                img.style.width = '80px';
-                
-                document.getElementById('images').appendChild(img);
-            } 
-        });
-    }
-});
+    // Reads the json 
+    var myItems;
+    $.getJSON('/ajax/v1-diagramdata.php', function(data) {
+        myItems = data;
+
+        $.each(data, function(key, val) {
+            var img = new Image();
+            img.src = val.venderlogoURL;
+            img.title = val.vendordesc;
+            img.style.margin = '5px 5px';
+            img.style.width = '40px';
+            console.log(val.venderlogoURL);
+            var type = val.sgid;
+            document.getElementById(type).appendChild(img);
+        })
+
+        console.log(myItems);
+    });
 };
 
 $(function() {
     displayAllImages();   
 });
+
